e69eb4159467905efd6e79aa863dbd192e0f53e0
Vincent Jacques <vincent@vincent-jacques.net>
2012-04-16 19:39:01 +0100

    Put DjangoWebsite in vincent-jacques.net.git

--- DjangoWebsite/QuadProgMm/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-urlpatterns = patterns( "",
-    ( r'^$', 'QuadProgMm.views.index' ),
-    ( r'^compile$', 'QuadProgMm.views.compile' ),
-    ( r'^generate$', 'QuadProgMm.views.generate' )
-)
--- DjangoWebsite/manage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import django.core.management
-import settings
-
-django.core.management.execute_manager( settings )
