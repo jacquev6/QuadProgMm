bf04ae3270236081ba1c86b6e8ca64fedfc80c22
Vincent Jacques <vincent@vincent-jacques.net>
2018-08-30 09:22:53 +0200

    Make Forms-test more systematic

--- src/QP/Forms-test.cc
+++ src/QP/Forms-test.cc
@@ -5,31 +5,44 @@
 #include "Forms.hpp"
 using namespace QP;
 
-void expect_linear_form(const LinearForm&) {}
-void expect_quadratic_form(const QuadraticForm&) {}
+void expect_linear_form_(const LinearForm&) {}
+void expect_quadratic_form_(const QuadraticForm&) {}
+void expect_constraint_(const Constraint&) {}
+
+#define EXPECT_LINEAR_FORM(x) expect_linear_form_(x); expect_quadratic_form_(x);
+#define EXPECT_QUADRATIC_FORM(x) expect_quadratic_form_(x);
+#define EXPECT_CONSTRAINT(x) expect_constraint_(x);
 
 BOOST_AUTO_TEST_CASE(Conversions) {
+  double d = 0;
   Variable v;
   LinearForm l = 0;
   QuadraticForm q = 0;
 
-  l = 0;
+  EXPECT_LINEAR_FORM(d);
+  EXPECT_LINEAR_FORM(v);
+  EXPECT_LINEAR_FORM(l);
+  EXPECT_QUADRATIC_FORM(q);
+
+  // N/A: d = d;
+  // N/A: d = v;
+  // N/A: d = l;
+  // N/A: d = q;
+
+  // N/A: v = d;
+  // N/A: v = v;
+  // N/A: v = l;
+  // N/A: v = q;
+
+  l = d;
   l = v;
   l = l;
+  // N/A: l = q;
 
-  q = 0;
+  q = d;
   q = v;
   q = l;
   q = q;
-
-  expect_linear_form(0);
-  expect_linear_form(v);
-  expect_linear_form(l);
-
-  expect_quadratic_form(0);
-  expect_quadratic_form(v);
-  expect_quadratic_form(l);
-  expect_quadratic_form(q);
 }
 
 BOOST_AUTO_TEST_CASE(UnarySignOperators) {
@@ -37,112 +50,226 @@ BOOST_AUTO_TEST_CASE(UnarySignOperators) {
   LinearForm l = 0;
   QuadraticForm q = 0;
 
-  expect_linear_form(-v);
-  expect_linear_form(+v);
-  expect_linear_form(-l);
-  expect_linear_form(+l);
-
-  expect_quadratic_form(-v);
-  expect_quadratic_form(+v);
-  expect_quadratic_form(-l);
-  expect_quadratic_form(+l);
-  expect_quadratic_form(-q);
-  expect_quadratic_form(+q);
+  EXPECT_LINEAR_FORM(+v);
+  EXPECT_LINEAR_FORM(-v);
+  EXPECT_LINEAR_FORM(+l);
+  EXPECT_LINEAR_FORM(-l);
+  EXPECT_QUADRATIC_FORM(+q);
+  EXPECT_QUADRATIC_FORM(-q);
 }
 
 BOOST_AUTO_TEST_CASE(AdditionOperators) {
+  double d = 0;
   Variable v;
   LinearForm l = 0;
   QuadraticForm q = 0;
 
-  expect_linear_form(v + v);
-  expect_linear_form(v - v);
-  expect_linear_form(v + 1);
-  expect_linear_form(v - 1);
-  expect_linear_form(1 + v);
-  expect_linear_form(1 - v);
+  // N/A: d d
+  // d v
+  // N/A: d += v;
+  // N/A: d -= v;
+  EXPECT_LINEAR_FORM(d + v);
+  EXPECT_LINEAR_FORM(d - v);
+  // d l
+  // N/A: d += l;
+  // N/A: d -= l;
+  EXPECT_LINEAR_FORM(d + l);
+  EXPECT_LINEAR_FORM(d - l);
+  // d q
+  // N/A: d += q;
+  // N/A: d -= q;
+  EXPECT_QUADRATIC_FORM(d + q);
+  EXPECT_QUADRATIC_FORM(d - q);
 
-  l += l;
-  l -= l;
+  // v d
+  // N/A: v += d;
+  // N/A: v -= d;
+  EXPECT_LINEAR_FORM(v + d);
+  EXPECT_LINEAR_FORM(v - d);
+  // v v
+  // N/A: v += v;
+  // N/A: v -= v;
+  EXPECT_LINEAR_FORM(v + v);
+  EXPECT_LINEAR_FORM(v - v);
+  // v l
+  // N/A: v += l;
+  // N/A: v -= l;
+  EXPECT_LINEAR_FORM(v + l);
+  EXPECT_LINEAR_FORM(v - l);
+  // v q
+  // N/A: v += q;
+  // N/A: v -= q;
+  EXPECT_QUADRATIC_FORM(v + q);
+  EXPECT_QUADRATIC_FORM(v - q);
+
+  // l d
+  l += d;
+  l -= d;
+  EXPECT_LINEAR_FORM(l + d);
+  EXPECT_LINEAR_FORM(l - d);
+  // l v
   l += v;
   l -= v;
-  l += 1;
-  l -= 1;
-  expect_linear_form(l + l);
-  expect_linear_form(l - l);
-  expect_linear_form(l + v);
-  expect_linear_form(l - v);
-  expect_linear_form(v + l);
-  expect_linear_form(v - l);
-  expect_linear_form(l + 1);
-  expect_linear_form(l - 1);
-  expect_linear_form(1 + l);
-  expect_linear_form(1 - l);
+  EXPECT_LINEAR_FORM(l + v);
+  EXPECT_LINEAR_FORM(l - v);
+  // l l
+  l += l;
+  l -= l;
+  EXPECT_LINEAR_FORM(l + l);
+  EXPECT_LINEAR_FORM(l - l);
+  // l q
+  // N/A: l += q;
+  // N/A: l -= q;
+  EXPECT_QUADRATIC_FORM(l + q);
+  EXPECT_QUADRATIC_FORM(l - q);
 
-  q += q;
-  q -= q;
-  q += l;
-  q -= l;
+  // q d
+  q += d;
+  q -= d;
+  EXPECT_QUADRATIC_FORM(q + d);
+  EXPECT_QUADRATIC_FORM(q - d);
+  // q v
   q += v;
   q -= v;
-  q += 1;
-  q -= 1;
-  expect_quadratic_form(v + v);
-  expect_quadratic_form(v - v);
-  expect_quadratic_form(v + 1);
-  expect_quadratic_form(v - 1);
-  expect_quadratic_form(1 + v);
-  expect_quadratic_form(1 - v);
-
-  expect_quadratic_form(l + l);
-  expect_quadratic_form(l - l);
-  expect_quadratic_form(l + v);
-  expect_quadratic_form(l - v);
-  expect_quadratic_form(v + l);
-  expect_quadratic_form(v - l);
-  expect_quadratic_form(l + 1);
-  expect_quadratic_form(l - 1);
-  expect_quadratic_form(1 + l);
-  expect_quadratic_form(1 - l);
-
-  expect_quadratic_form(q + q);
-  expect_quadratic_form(q - q);
-  expect_quadratic_form(q + l);
-  expect_quadratic_form(q - l);
-  expect_quadratic_form(l + q);
-  expect_quadratic_form(l - q);
-  expect_quadratic_form(q + v);
-  expect_quadratic_form(q - v);
-  expect_quadratic_form(v + q);
-  expect_quadratic_form(v - q);
-  expect_quadratic_form(q + 1);
-  expect_quadratic_form(q - 1);
-  expect_quadratic_form(1 + q);
-  expect_quadratic_form(1 - q);
+  EXPECT_QUADRATIC_FORM(q + v);
+  EXPECT_QUADRATIC_FORM(q - v);
+  // q l
+  q += l;
+  q -= l;
+  EXPECT_QUADRATIC_FORM(q + l);
+  EXPECT_QUADRATIC_FORM(q - l);
+  // q q
+  q += q;
+  q -= q;
+  EXPECT_QUADRATIC_FORM(q + q);
+  EXPECT_QUADRATIC_FORM(q - q);
 }
 
 BOOST_AUTO_TEST_CASE(MultiplicationOperators) {
+  double d = 0;
   Variable v;
   LinearForm l = 0;
   QuadraticForm q = 0;
 
-  expect_linear_form(v * 1);
-  expect_linear_form(v / 1);
-  expect_linear_form(1 * v);
+  // N/A: d d
+  // d v
+  // N/A: d *= v;
+  // N/A: d /= v;
+  EXPECT_LINEAR_FORM(d * v);
+  // N/A: d / v;
+  // d l
+  // N/A: d *= l;
+  // N/A: d /= l;
+  EXPECT_LINEAR_FORM(d * l);
+  // N/A: d / l;
+  // d q
+  // N/A: d *= q;
+  // N/A: d /= q;
+  EXPECT_QUADRATIC_FORM(d * q);
+  // N/A: d / q;
 
-  l *= 1;
-  l /= 1;
-  expect_linear_form(l * 1);
-  expect_linear_form(l / 1);
-  expect_linear_form(1 * l);
+  // v d
+  // N/A: v *= d;
+  // N/A: v /= d;
+  EXPECT_LINEAR_FORM(v * d);
+  EXPECT_LINEAR_FORM(v / d);
+  // v v
+  // N/A: v *= v;
+  // N/A: v /= v;
+  EXPECT_QUADRATIC_FORM(v * v);
+  // N/A: v / v;
+  // v l
+  // N/A: v *= l;
+  // N/A: v /= l;
+  EXPECT_QUADRATIC_FORM(v * l);
+  // N/A: v / l;
+  // v q
+  // N/A: v *= q;
+  // N/A: v /= q;
+  // N/A: v * q;
+  // N/A: v / q;
 
-  q *= 1;
-  q /= 1;
-  expect_quadratic_form(q * 1);
-  expect_quadratic_form(q / 1);
-  expect_quadratic_form(1 * q);
-  expect_quadratic_form(l * l);
-  expect_quadratic_form(l * v);
-  expect_quadratic_form(v * l);
-  expect_quadratic_form(v * v);
+  // l d
+  l *= d;
+  l /= d;
+  EXPECT_LINEAR_FORM(l * d);
+  EXPECT_LINEAR_FORM(l / d);
+  // l v
+  // N/A: l *= v;
+  // N/A: l /= v;
+  EXPECT_QUADRATIC_FORM(l * v);
+  // N/A: l / v;
+  // l l
+  // N/A: l *= l;
+  // N/A: l /= l;
+  EXPECT_QUADRATIC_FORM(l * l);
+  // N/A: l / l;
+  // l q
+  // N/A: l *= q;
+  // N/A: l /= q;
+  // N/A: l * q;
+  // N/A: l / q;
+
+  // q d
+  q *= d;
+  q /= d;
+  EXPECT_QUADRATIC_FORM(q * d);
+  EXPECT_QUADRATIC_FORM(q / d);
+  // q v
+  // N/A: q *= v;
+  // N/A: q /= v;
+  // N/A: q * v;
+  // N/A: q / v;
+  // q l
+  // N/A: q *= l;
+  // N/A: q /= l;
+  // N/A: q * l;
+  // N/A: q / l;
+  // q q
+  // N/A: q *= q;
+  // N/A: q /= q;
+  // N/A: q * q;
+  // N/A: q / q;
+}
+
+BOOST_AUTO_TEST_CASE(ComparisonOperators) {
+  double d = 0;
+  Variable v;
+  LinearForm l = 0;
+
+  // N/A: d d
+  // d v
+  EXPECT_CONSTRAINT(d == v)
+  EXPECT_CONSTRAINT(d >= v)
+  EXPECT_CONSTRAINT(d <= v)
+  // d l
+  EXPECT_CONSTRAINT(d == l)
+  EXPECT_CONSTRAINT(d >= l)
+  EXPECT_CONSTRAINT(d <= l)
+
+  // v d
+  EXPECT_CONSTRAINT(v == d)
+  EXPECT_CONSTRAINT(v >= d)
+  EXPECT_CONSTRAINT(v <= d)
+  // v v
+  EXPECT_CONSTRAINT(v == v)
+  EXPECT_CONSTRAINT(v >= v)
+  EXPECT_CONSTRAINT(v <= v)
+  // v l
+  EXPECT_CONSTRAINT(v == l)
+  EXPECT_CONSTRAINT(v >= l)
+  EXPECT_CONSTRAINT(v <= l)
+
+  // l d
+  EXPECT_CONSTRAINT(l == d)
+  EXPECT_CONSTRAINT(l >= d)
+  EXPECT_CONSTRAINT(l <= d)
+  // l v
+  EXPECT_CONSTRAINT(l == v)
+  EXPECT_CONSTRAINT(l >= v)
+  EXPECT_CONSTRAINT(l <= v)
+  // l l
+  EXPECT_CONSTRAINT(l == l)
+  EXPECT_CONSTRAINT(l >= l)
+  EXPECT_CONSTRAINT(l <= l)
 }
